8c4f325ea22161001dd76902a4f24ae3dffa9af2
Ricardo Molinas <ricardo121990@gmail.com>
2019-07-17 20:33:02 -0400

    cambios array

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+BASADO EN https://dolar.melizeche.com/
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -66,3 +69,8 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+### Dependencias
+npm install react-currency-format --save
+
--- src/CambiosList.js
+++ src/CambiosList.js
@@ -13,8 +13,9 @@ function TasaCambio(props) {
 }
 
 function PrintFormat(props){
-    const cotizacion = Number((props.cotizacion).toFixed(2));
-    return (<CurrencyFormat value={cotizacion}
+    const cotizacion = props.cotizacion;
+    const monto = Number((cotizacion * props.monto).toFixed(2));
+    return (<CurrencyFormat value={monto}
         decimalSeparator={','}
         thousandSeparator={'.'}
         displayType={'text'}
@@ -28,11 +29,11 @@ class CambiosList extends React.Component {
 
         this.state = {
             bancos: [],
+            nombres: [],
             loading: true,
             error: null,
             updated: null,
             monto: 1, 
-            setcambio: 1,
         };
         this.handleChange = this.handleChange.bind(this);
     }
@@ -40,7 +41,6 @@ class CambiosList extends React.Component {
     componentWillUnmount() {
         clearInterval(this.intervalId);
     }
-
 
     componentDidMount() {
         this.setState({ loading: true, error: null });
@@ -55,45 +55,26 @@ class CambiosList extends React.Component {
             })
             .then(data => {
                 this.setState({
-                    amambayCompra: (data.dolarpy.amambay.compra * this.state.monto),
-                    amambayVenta: (data.dolarpy.amambay.venta * this.state.monto),
-                    bbvaCompra: (data.dolarpy.bbva.compra * this.state.monto),
-                    bbvaVenta: (data.dolarpy.bbva.venta * this.state.monto),
-                    bcpCompra: (data.dolarpy.bcp.compra * this.state.monto),
-                    bcpVenta: (data.dolarpy.bcp.venta * this.state.monto),
-                    bcpReferencial: (data.dolarpy.bcp.referencial_diario * this.state.monto),
-                    cambiosalberdiCompra: (data.dolarpy.cambiosalberdi.compra * this.state.monto),
-                    cambiosalberdiVenta: (data.dolarpy.cambiosalberdi.venta * this.state.monto),
-                    cambioschacoCompra: (data.dolarpy.cambioschaco.compra * this.state.monto),
-                    cambioschacoVenta: (data.dolarpy.cambioschaco.venta * this.state.monto),
-                    eurocambiosCompra: (data.dolarpy.eurocambios.compra * this.state.monto),
-                    eurocambiosVenta: (data.dolarpy.eurocambios.venta * this.state.monto),
-                    interfisaCompra: (data.dolarpy.interfisa.compra * this.state.monto),
-                    interfisaVenta: (data.dolarpy.interfisa.venta * this.state.monto),    
-                    setCompra: (data.dolarpy.set.compra * this.state.monto),
-                    setVenta: (data.dolarpy.set.venta * this.state.monto),  
-                    mydcambiosCompra: (data.dolarpy.mydcambios.compra * this.state.monto),
-                    mydcambiosVenta: (data.dolarpy.mydcambios.venta * this.state.monto),  
-                    maxicambiosCompra: (data.dolarpy.maxicambios.compra * this.state.monto),
-                    maxicambiosVenta: (data.dolarpy.maxicambios.venta * this.state.monto),                
+                    bancos: [(data.dolarpy.bbva), (data.dolarpy.bcp), (data.dolarpy.cambiosalberdi),
+                        (data.dolarpy.cambioschaco), (data.dolarpy.eurocambios), (data.dolarpy.interfisa), (data.dolarpy.set),
+                        (data.dolarpy.mydcambios), (data.dolarpy.maxicambios)],
                     loading: false,
                     updated: data.updated
                 });
-            });
+                
+            })
+            .catch(error => this.setState({ error, loading: false }));
     }
 
     handleChange(event) {
         this.setState({ monto: event.target.value });
         this.fetchData()
     }
-    handleClick = e => {
-        this.setState({ monto: 1 });
-    }
 
 
     render() {
         const monto = this.state.monto
-        
+        const bancos = this.state.bancos
         if (this.state.error) {
             return <div> Error: {this.state.error.message}</div>;
         }else if (this.state.loading) {
@@ -109,127 +90,33 @@ class CambiosList extends React.Component {
                                 type="text"
                                 value={this.state.monto}
                             />
-                            <CurrencyFormat value={this.state.monto}
-                                decimalSeparator={','}
-                                thousandSeparator={'.'}
-                                displayType={'text'}
-                                prefix={'$ '} /> 
+                            <PrintFormat cotizacion={this.state.monto} monto={1} />
                             <br/>
                             <TasaCambio cambios={monto}/>
                         </div>
                         <CardColumns>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>BBVA</p>
-                                        <PrintFormat cotizacion={this.state.bbvaCompra} /> 
-                                        
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.bbvaVenta} /> 
-                                                                         
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>Amambay</p>
-                                        <PrintFormat cotizacion={this.state.amambayCompra} /> 
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.amambayVenta} /> 
-
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>BCP</p>
-                                        <PrintFormat cotizacion={this.state.bcpCompra} /> 
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.bcpVenta} /> 
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.bcpReferencial} />  
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>Cambios Alberdi</p>
-                                        <PrintFormat cotizacion={this.state.cambiosalberdiCompra} />  
-                                    <br />
-                                        <PrintFormat cotizacion={this.state.cambiosalberdiVenta} />  
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>Cambios Chaco</p>
-                                        <PrintFormat cotizacion={this.state.cambioschacoCompra} />  
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.cambiosalberdiVenta} />  
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>Euro cambios</p>
-                                        <PrintFormat cotizacion={this.state.eurocambiosCompra} /> 
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.eurocambiosVenta} /> 
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>Interfisa</p>
-                                        <PrintFormat cotizacion={this.state.interfisaCompra} /> 
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.interfisaVenta} /> 
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>Maxi Cambios</p>
-                                        <PrintFormat cotizacion={this.state.maxicambiosCompra} /> 
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.maxicambiosVenta} /> 
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>M y d Cambios</p>
-                                        <PrintFormat cotizacion={this.state.mydcambiosCompra} /> 
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.mydcambiosVenta} /> 
-                                    </center>
-                                </Card.Title>
-                            </Card>
-                            <Card className="p-2">
-                                <Card.Title>
-                                    <center>
-                                        <p>SET</p>
-                                        <PrintFormat cotizacion={this.state.setCompra} /> 
-                                        <br />
-                                        <PrintFormat cotizacion={this.state.setVenta} /> 
-                                    </center>
-                                </Card.Title>
-                            </Card>
-
+                            <ul className="list-unstyled">
+                                {bancos.map((number,index)=>
+                                    <li key={index}>
+                                        <Card className="p-2">
+                                            <Card.Title>
+                                                <center>
+                                                <p>{index}</p>
+                                                    <PrintFormat cotizacion={number.compra} monto={monto} />
+                                                <br/>
+                                                    <PrintFormat cotizacion={number.venta} monto={monto} /> 
+                                                </center>
+                                            </Card.Title>
+                                        </Card>
+                                            
+                                    </li>
+                                )}
+                            </ul>
                         </CardColumns>
                         <footer id="sticky-footer" className="py-4 bg-dark text-white-50">
                             <div className="container text-center">
                                 <p>Última actualización: {this.state.updated}</p>
-                                {/* <p>{this.state.updated}</p> */}
-                            </div>
+=                            </div>
                         </footer>                        
                     </div>
                 )}
